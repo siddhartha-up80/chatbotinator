0cbc236e49992afedb84695f19ae1c385d60a319
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-02-08 22:40:17 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | AI powered Inators",
+    default: "AI powered Inators",
+  },
+  description: "Checkout AI powered Inators",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">
+          <Navbar />
+          <main className="mx-auto max-w-3xl px-3 py-10">{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
